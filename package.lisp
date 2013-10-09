7c9f50d9a744cbb22b36f2e39dc7939d6e9e558c
Timofei Shatrov <timofei.shatrov@gmail.com>
2013-10-09 18:36:03 +0400

    fix packages

--- package.lisp
+++ package.lisp
@@ -2,9 +2,10 @@
 
 (defpackage :webgunk
   (:use :cl :alexandria)
-  (:export :http-request :parse-url :parse-request :get-attributes
-           :node-text :url-params :get-url-param :strip-whitespace
-           :jsown-filter)
+  (:export :jsown-filter :get-attributes :strip-whitespace :node-text
+           :http-request :parse-url :parse-request
+           :url-params :get-url-param :append-param-str 
+           :url-params-to-string :make-url)
   (:import-from :split-sequence :split-sequence))
 
 (defpackage :webgunk/test
@@ -18,4 +19,4 @@
   (:use :cl :webgunk :webgunk/modules))
 
 (defpackage :webgunk/reddit
-  (:use :cl :webgunk :webgunk/modules))
+  (:use :cl :alexandria :webgunk :webgunk/modules))
