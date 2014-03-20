ce9cd4bfa76ed62a371212924e73de045b1407e5
Timofei Shatrov <timofei.shatrov@gmail.com>
2014-03-20 18:47:51 +0400

    more fun with tumblr notes

--- tumblr.lisp
+++ tumblr.lisp
@@ -22,22 +22,40 @@
    (type :initarg :type :reader note-type) ; :like or :reblog
    (user :initarg :user :reader note-user)
    (from :initarg :from :initform nil :reader note-from)
-   (link :initarg :link :initform nil :reader note-link)))
+   (link :initarg :link :initform nil :reader note-link)
+   (comment :initarg :comment :initform nil :reader note-comment)))
 
+
+(defmethod print-object ((obj tumblr-note) stream)
+  (print-unreadable-object (obj stream :type t)
+    (format stream "~a ~a~@[ :FROM ~a~]~:[~; :WITHCOMMENT~]"
+            (note-type obj) (note-user obj) (note-from obj) (note-comment obj))))
 
 (defun make-note-from-node (node)
-  (loop with note-type and note-user
+  (loop with note-type and note-user and note-comment and note-link and note-from
      for class in (class-list node)
      do (cond 
           ((equal class "more_notes_link_container") (return))
           ((equal class "like")
            (setf note-type :like))
+          ((equal class "original_post")
+           (setf note-type :original))
           ((equal class "reblog")
-           (setf note-type :reblog))
+           (setf note-type :reblog)
+           (let ((source-node (car (css:query "a.source_tumblelog" node))))
+             (when source-node
+               (setf note-from (node-text source-node)))))
+          ((equal class "with_commentary")
+           (let ((comment-node (car (css:query "blockquote > a" node))))
+             (when comment-node
+               (setf note-comment (node-text comment-node))
+               (setf note-link (dom:get-attribute comment-node "href")))))
           (t (let ((user (nth-value 1 (starts-with-subseq "tumblelog_" class :return-suffix t))))
                (if user (setf note-user user)))))
      finally (return (make-instance 'tumblr-note 
-                                    :text (node-text node) :type note-type :user note-user))))
+                                    :text (node-text node) :type note-type :user note-user
+                                    :comment note-comment :link note-link :from note-from
+                                    ))))
      
 
 ;; tumblrReq.open('GET','/notes/801#########/wwwwwwwww?from_c=##########',true);
@@ -70,3 +88,10 @@
 (defmethod get-all-notes ((obj tumblr-post))
   (loop while (get-notes-once obj) do (princ ".")))
       
+
+(defun get-post-notes (url)
+  (let ((post (make-instance 'tumblr-post :url url)))
+    (get-all-notes post)
+    post))
+
+  
